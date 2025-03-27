03f70c5344e8d766ec5cc8787911138415539d77
Riya Kumari <148714238+riyakumari8@users.noreply.github.com>
2025-03-27 08:32:50 +0530

    Add files via upload

---
+++ Notes_App.java
@@ -0,0 +1,20 @@
+public class notesApp {
+    static final String FILE_NAME = "notes.txt";
+
+    //write a new note
+
+
+    //read notes
+
+    //delete all notes
+
+    //modify notes
+    public static void main(String[] args) {
+        //take user input on choices
+
+        // 1-> write note
+        // 2 -> read note
+        // 3 -> delete notes
+        // 4-> exit
+    }
+}
